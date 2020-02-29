e6640a077cf204ac8309d454b518fc22eeca7935
Jose <josemulamorales33@hotmail.com>
2020-02-29 12:22:26 +0000

    Create Script Google Searach bar Chrome/Firefox

--- src/google_search/TC1Google_search_Chrome.java
+++ src/google_search/TC1Google_search_Chrome.java
@@ -10,26 +10,19 @@ public class TC1Google_search_Chrome {
     public static void main(String[] args) {
         // Auto-generated method stub
     	System.setProperty("webdriver.chrome.driver", "/....../chromedriver");
-
         // Initialise browser
         WebDriver driver = new ChromeDriver();
-
         // Open Google
         driver.get("https://www.google.com/");
-
         // Maximise browser
         driver.manage().window().maximize();
       	
       	//Get the WebElement into search bar
         WebElement bar = driver.findElement(By.name("q"));
-        
         bar.sendKeys("Guru99");
         
         //Click WebElement search button
         WebElement search = driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[3]/center/input[1]"));
-        
         search.click();
-        
     }  
-
 }

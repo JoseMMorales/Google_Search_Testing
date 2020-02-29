0115ce62709be4f1a5a12e56c266f2e5fe3a828f
Jose <josemulamorales33@hotmail.com>
2020-02-29 13:16:21 +0000

    Update Script Google ChromeDriver

--- src/google_search/TC1Google_search_Chrome.java
+++ src/google_search/TC1Google_search_Chrome.java
@@ -8,14 +8,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TC1Google_search_Chrome {
 
     public static void main(String[] args) {
-        // Auto-generated method stub
-    	System.setProperty("webdriver.chrome.driver", "/....../chromedriver");
-        // Initialise browser
-        WebDriver driver = new ChromeDriver();
-        // Open Google
-        driver.get("https://www.google.com/");
-        // Maximise browser
-        driver.manage().window().maximize();
+    	System.setProperty("webdriver.chrome.driver", "/....../chromedriver"); 
+        WebDriver driver = new ChromeDriver();  // Initiate browser
+        driver.get("https://www.google.com/");  // Open Google
+        driver.manage().window().maximize();    // Maximise browser
       	
       	//Get the WebElement into search bar
         WebElement bar = driver.findElement(By.name("q"));

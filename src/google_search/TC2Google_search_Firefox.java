19d0bf267c27ca5934a25eaaf2007452c46c03f2
Jose <josemulamorales33@hotmail.com>
2020-02-29 12:34:38 +0000

    Update Script Google Search Bar Firefox

--- src/google_search/TC2Google_search_Firefox.java
+++ src/google_search/TC2Google_search_Firefox.java
@@ -10,26 +10,19 @@ public class TC2Google_search_Firefox {
     public static void main(String[] args) {
         // Auto-generated method stub
     	System.setProperty("webdriver.gecko.driver", "/......./geckodriver");
-
         // Initialise browser
         WebDriver driver = new FirefoxDriver();
-
         // Open Google
         driver.get("https://www.google.com/");
-
         // Maximise browser
         driver.manage().window().maximize();
       	
       	//Get the WebElement into search bar
         WebElement bar = driver.findElement(By.name("q"));
-        
         bar.sendKeys("Guru99");
         
         //Click WebElement search button
         WebElement search = driver.findElement(By.xpath("/html/body/div/div[4]/form/div[2]/div[1]/div[3]/center/input[1]"));
-        
         search.click();
-        
-    }  
-
+    } 
 }

7a3fe6b8ba86e607b6db15ed252328fe6e4d9a45
Jose <josemulamorales33@hotmail.com>
2020-03-01 12:41:46 +0000

    Create Google Search bar JUnit (Chrome/Firefox)

--- src:/JUnit/TC1GoogleSearchChrome.java
+++ src:/JUnit/TC1GoogleSearchChrome.java
@@ -13,39 +13,35 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TC1GoogleSearchChrome {
-	
-	private WebDriver driver;
-	
-	By BarLocator = By.name("q");
-	By SearchButtonLocator = By.name("btnK");
-	
+  
+  private WebDriver driver;
+  By BarLocator = By.name("q");
+  By SearchButtonLocator = By.name("btnK");
+  
+  @Before
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "/Users/jose/Desktop/chromedriver");
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("https://www.google.com/");
+  }
+  
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+  }
 
-	@Before
-	public void setUp() throws Exception {
-		
-		System.setProperty("webdriver.chrome.driver", "/Users/jose/Desktop/chromedriver");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.google.com/");
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		driver.quit();
-	}
-
-	@Test
-	public void test() throws InterruptedException {
-		Thread.sleep(2000);
-		if(driver.findElement(BarLocator).isDisplayed()) {
-			driver.findElement(BarLocator).sendKeys("Guru99");
-			driver.findElement(BarLocator).click();
-			driver.findElement(SearchButtonLocator).click();
-		}else {
-			System.out.println("Search Bar is not displayed!");
-		}
-		List<WebElement> H3Text = driver.findElements(By.tagName("h3"));
-		
-		assertEquals("Meet Guru99 - Free Training Tutorials & Video for IT Courses", H3Text.get(0).getText());	
-	}
+  @Test
+  public void test() throws InterruptedException {
+    Thread.sleep(2000);
+    if(driver.findElement(BarLocator).isDisplayed()) {
+      driver.findElement(BarLocator).sendKeys("Guru99");
+      driver.findElement(BarLocator).click();
+      driver.findElement(SearchButtonLocator).click();
+    }else {
+      System.out.println("Search Bar is not displayed!");
+    }
+    List<WebElement> H3Text = driver.findElements(By.tagName("h3"));
+    assertEquals("Meet Guru99 - Free Training Tutorials & Video for IT Courses", H3Text.get(0).getText());	
+  }
 }

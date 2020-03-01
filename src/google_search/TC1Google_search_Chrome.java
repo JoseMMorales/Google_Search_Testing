e9ef3de34fce15137acf60ff9193181b7f03fa5b
Jose <josemulamorales33@hotmail.com>
2020-03-01 12:23:33 +0000

    Update Google Search bar (Chrome/Firefox)

--- src/google_search/TC1Google_search_Chrome.java
+++ src/google_search/TC1Google_search_Chrome.java
@@ -8,12 +8,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TC1Google_search_Chrome {
 
     public static void main(String[] args) {
-      
+
     	System.setProperty("webdriver.chrome.driver", "/....../chromedriver"); 
         WebDriver driver = new ChromeDriver();  // Initiate browser
         driver.get("https://www.google.com/");  // Open Google
         driver.manage().window().maximize();    // Maximise browser
-      	
+        
       	//Get the WebElement into search bar
         WebElement bar = driver.findElement(By.name("q"));
         bar.sendKeys("Guru99");

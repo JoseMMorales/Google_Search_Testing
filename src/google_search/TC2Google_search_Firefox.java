5ee6fe5d57e3a0682434165e8101f35601371668
Jose <josemulamorales33@hotmail.com>
2020-03-01 12:02:45 +0000

    Update Script Google Firefox

--- src/google_search/TC2Google_search_Firefox.java
+++ src/google_search/TC2Google_search_Firefox.java
@@ -8,8 +8,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class TC2Google_search_Firefox {
 
     public static void main(String[] args) {
+      
     	System.setProperty("webdriver.gecko.driver", "/......./geckodriver");
-        WebDriver driver = new FirefoxDriver();   // Initialise browser
+        WebDriver driver = new FirefoxDriver();   // Initiate browser
         driver.get("https://www.google.com/");    // Open Google
         driver.manage().window().maximize();      // Maximise browser
       	

58efbf7164069a1d537439a80d2c2ecd91925af9
Jose <josemulamorales33@hotmail.com>
2020-03-01 12:01:20 +0000

    Update Script Google ChromeDriver

--- src/google_search/TC1Google_search_Chrome.java
+++ src/google_search/TC1Google_search_Chrome.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TC1Google_search_Chrome {
 
     public static void main(String[] args) {
+      
     	System.setProperty("webdriver.chrome.driver", "/....../chromedriver"); 
         WebDriver driver = new ChromeDriver();  // Initiate browser
         driver.get("https://www.google.com/");  // Open Google
